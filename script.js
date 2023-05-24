20c116aa6935b1e0463d98984e43ab3080e12bfc
Albert Kemp <91766342+albertkemp@users.noreply.github.com>
2023-05-24 17:56:25 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const imageContainer = document.getElementById('imageContainer');
+const closeButton = document.getElementById('closeButton');
+
+closeButton.addEventListener('click', function() {
+  imageContainer.style.display = 'none';
+});
